9e2df4ba4a9ac9a3b9f0adec0b31463b99bca6a9
Luiz Fernando Glau Vaz <luizglauvaz@hotmail.com>
2019-11-22 09:36:29 -0300

    check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,24 +21,24 @@ public class Program {
 		
 		while(true) {
 			try {
-			UI.clearScreen();
-			UI.printMatch(chessMatch, captured);
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
+				UI.clearScreen();
+				UI.printMatch(chessMatch, captured);
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
 			
-			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces(), possibleMoves);
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
-			if (capturedPiece !=null) {
-				captured.add(capturedPiece);
-			}
+				if (capturedPiece !=null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
@@ -47,7 +47,7 @@ public class Program {
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
+			}	
 		}
 	}
 }
